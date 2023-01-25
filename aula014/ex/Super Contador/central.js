7313ddfe97153fff2bff3777bf1314c93720e12a
alefalves4867 <123469430+alefalves4867@users.noreply.github.com>
2023-01-24 21:31:54 -0300

    update

--- aula014/ex/Super Contador/central.js
+++ aula014/ex/Super Contador/central.js
@@ -5,7 +5,7 @@ var pass = Number((document.querySelector('#pss')).value)
 var res = document.querySelector('#res')
 
 var inc2 = inc
-if (inc == fin || pass == fin || pass == 0){
+if (inc == fin || pass == fin){
     window.alert('[ERROR] Tente Novamente')
 
 } else if (inc <= -1 && pass <= -1 && fin >= 1){
@@ -14,7 +14,10 @@ if (inc == fin || pass == fin || pass == 0){
 } else {
     res.innerHTML = `<p>Contando:</p>${inc} &#128073;`
     while (inc <= fin) {
-
+        if (pass == 0){
+            window.alert('Passo inválido! Considerando passo = 1')
+            pass = 1
+        }
         inc += pass
         if (inc <= fin){
         res.innerHTML += `${inc} &#128073;`
