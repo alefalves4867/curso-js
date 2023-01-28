02d0058562c4fd2f77bc0a6e5756d3b326340a8b
alefalves4867 <123469430+alefalves4867@users.noreply.github.com>
2023-01-28 20:02:27 -0300

    atualização final

--- aula016/ex/num.js
+++ aula016/ex/num.js
@@ -1,8 +1,10 @@
 let numero = []
 function adicionar(){
+    let fn = document.getElementById('final')
     let n = document.getElementById('num')
     let tab = document.getElementById('lista')
     let num = Number(n.value)
+    
     if(n.value.length == 0){
         window.alert('Digite um número!')
     } else if (n.value > 100 || n.value < 1){
@@ -15,21 +17,37 @@ function adicionar(){
 
         item.text = `Valor ${num} adicionado`
         tab.appendChild(item)
+        fn.innerHTML = ''
     }
-   
+    n.value = ''
+    n.focus()
     
 }
 function finalizar(){
-    let c = numero.length
-    let fn = document.getElementById('final')
-    numero.sort()
-    fn.innerHTML = `<p>Ao todo, temos ${c} números cadastrados.</p><br>`
-    fn.innerHTML += `<p>O menor valor encontrado foi ${numero[c-1]}.</p><br>`
-    fn.innerHTML += `<p>O maior valor encontrado foi ${numero[0]}.</p><br>`
-    let total = 0
-    for (var i = 0; i < c; i++){
-        total += numero[i]
+    if (numero.length == 0){
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = numero.length
+        let maior = numero[0]
+        let menor = numero[0]
+        let soma = 0
+        let media = 0
+        for(let pos in numero){
+            soma += numero[pos]
+            if (numero[pos] > maior){
+                maior = numero[pos]
+            }
+            if (numero[pos] < menor){
+                menor = numero[pos]
+            }
+        }    
+    media = soma/tot
+    let res = document.getElementById('final')   
+    res.innerHTML = ''
+    res.innerHTML += `<p>Ao todo, temos ${tot} número(s) cadastrados</p>`
+    res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+    res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+    res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+    res.innerHTML += `<p>A média entre todos os números é ${media}`
     }
-    fn.innerHTML += `<p>Somando todos os valores, temos ${total}.</p><br>`
-    fn.innerHTML += `<p>A média dos valores digitados é ${total/c}.</p><br>`
 }
