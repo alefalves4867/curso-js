f071b8a4a7897db0f6cd2cbb0e42dd2f3eb5fdfc
alefalves4867 <123469430+alefalves4867@users.noreply.github.com>
2023-02-03 15:18:54 -0300

    atualizei

---
+++ CFB/aula025/arrow_function.js
@@ -0,0 +1,10 @@
+function checar(){
+    let a = document.getElementById('inome').innerHTML
+    
+    if (a.toUpperCase() == "ALEF") {
+        window.alert('Bem vindo, My Lord!')
+    }else{
+        window.alert(a)
+    }
+}
+
