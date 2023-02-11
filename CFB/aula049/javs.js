57727399d1b8a05bf5f0a308dc09c77eb43fdb1c
alefalves4867 <123469430+alefalves4867@users.noreply.github.com>
2023-02-11 10:02:37 -0300

    reduce

---
+++ CFB/aula049/javs.js
@@ -0,0 +1,18 @@
+const p_array = document.querySelector('#array')
+const btnVerificar = document.querySelector('#btnVerificar')
+const resultado = document.querySelector('#resultado')
+
+const elementos = [1, 2, 3, 4, 5]
+let ant=[]
+let atu=[]
+
+p_array.innerHTML = `[${elementos}]`
+
+btnVerificar;addEventListener('click', ((evt)=>{
+   resultado.innerHTML=elementos.reduce((anterior, atual, pos) => {
+        ant.push(anterior)
+        atu.push(atual)
+        return atual + anterior
+   })
+   resultado.innerHTML+=`<br> V.anterior: ${ant} <br> V.atual: ${atu}`
+}))
